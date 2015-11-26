4c94062c7cb6fdea38ae2aeeecfa45c67a88ab0d
eolembrain-leo <eolembrain.leo@gmail.com>
2015-11-27 03:01:30 +0800

    update

--- try_item.R
+++ try_item.R
@@ -34,7 +34,7 @@ get_item_link <- function(link, p_data_num = 10000) {
     GET(page_link,
         user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36")
     )},
-    warning = function(w) {cat("FAIL!", page_link, "\n"); return(NULL)}
+    error = function(w) {cat("FAIL!", page_link, "\n"); return(NULL)}
   )
   if (is.null(res)) return(NULL)
 
@@ -53,7 +53,7 @@ get_item_data <- function(link) {
     GET(link,
         user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36")
     )},
-    warning = function(w) {cat("FAIL!", link, "\n"); return(NULL)}
+    error = function(w) {cat("FAIL!", link, "\n"); return(NULL)}
   )
   if (is.null(res)) return(NULL)
 
@@ -64,14 +64,21 @@ get_item_data <- function(link) {
   result <- list(
     item_name = xmlValue(node["//span[@id='prod_title']"][[1]]),
     pic = unname(unclass(node["//img[@id='item_img']/@src"][[1]])),
-    class_1 = xmlValue(node["//div[@class='siteMargin']/ul/li[3]/a"][[1]]),
-    class_2 = xmlValue(node["//div[@class='siteMargin']/ul/li[5]/a"][[1]]),
-    class_3 = xmlValue(node["//div[@class='siteMargin']/ul/li[7]/a"][[1]]),
+    class_1 = tryCatch(xmlValue(node["//div[@class='siteMargin']/ul/li[3]/a"][[1]]),
+                       error = function(w) {cat("('class_1' not exist)", link, "\n"); return(NA)}
+    ),
+    class_2 = tryCatch(xmlValue(node["//div[@class='siteMargin']/ul/li[5]/a"][[1]]),
+                       error = function(w) {cat("('class_2' not exist)", link, "\n"); return(NA)}
+    ),
+    class_3 = tryCatch(xmlValue(node["//div[@class='siteMargin']/ul/li[7]/a"][[1]]),
+                       error = function(w) {cat("('class_3' not exist)", link, "\n"); return(NA)}
+    ),
     price = stringr::str_extract(xmlValue(node["//span[@class='price_num']"][[1]]),
                                  "[0-9]+"),
-    spec = stringr::str_match(misc_text, "規格:(.*)\r\n")[,2],
-    made_in = stringr::str_match(misc_text, "產地:(.*)\r\n")[,2],
-    unit = stringr::str_match(misc_text, "單位:(.*)$")[,2]
+    spec = stringr::str_match(misc_text, "\u898f\u683c:(.*)\r\n")[,2],
+    made_in = stringr::str_match(misc_text, "\u7522\u5730:(.*)\r\n")[,2],
+    unit = stringr::str_match(misc_text, "\u55ae\u4f4d:(.*)$")[,2],
+    url = link
   )
 
   result
@@ -81,6 +88,86 @@ get_item_data <- function(link) {
 # run ---------------------------------------------------------------------
 
 item_links <- unlist(sapply(link_1 , get_item_link))
-data_result_list <- lapply(item_links, get_item_data)
-data_result <- dplyr::bind_rows(data_result_list)
-readr::write_csv(data_result, "result/data_result.csv")
+# save(item_links, file="temp/item_links.RData")
+# load("temp/item_links.RData")
+
+data_result_list_1 <- lapply(item_links[1:2000], get_item_data)
+# save(data_result_list_1, file="temp/data_result_list_1.RData")
+# load("temp/data_result_list_1.RData")
+data_result_list_2 <- lapply(item_links[2001:4000], get_item_data)
+# save(data_result_list_2, file="temp/data_result_list_2.RData")
+# load("temp/data_result_list_2.RData")
+data_result_list_3 <- lapply(item_links[4001:6000], get_item_data)
+# save(data_result_list_3, file="temp/data_result_list_3.RData")
+# load("temp/data_result_list_3.RData")
+data_result_list_4 <- lapply(item_links[6001:length(item_links)], get_item_data)
+# save(data_result_list_4, file="temp/data_result_list_4.RData")
+# load("temp/data_result_list_4.RData")
+
+data_result <- lapply(list(data_result_list_1,
+                           data_result_list_2,
+                           data_result_list_3,
+                           data_result_list_4),
+                      dplyr::bind_rows
+) %>% bind_rows
+
+# parse -------------------------------------------------------------------
+
+remove_unit <- function(x, spec) {
+  #   x <- "雪芙蘭草本修護洗髮乳- 滋養修護"
+  #   spec <- "650g/滋養修護"
+  if (length(spec)!=1 || is.na(spec)) return(x)
+
+  trim <- function (x) gsub("^\\s+|[ /-]+$", "", x)
+
+  ## clear spec
+  spec <- gsub("[#!$@~\u25ce]+|^[*]", "", spec)
+  spec <- gsub("\\*", "\\\\*", spec)
+  spec <- trim(spec)
+
+  match_string <- strsplit(spec, "/")[[1]]
+  pattern_2 <- paste0(match_string, collapse = "|")
+
+  ## clear x
+  x <- gsub("[#!,\uff0f\u25c6]|^[*]|\u2605+[^\u2605]*\u2605+|\u3010[^\u3011]+\u3011", "", x)
+
+  try({
+    if(!grepl(paste0("^",spec), x)) {
+      x <- gsub(paste0(spec, "$"), "", x)
+      x <- gsub(pattern_2, "", x)
+    }
+  }, silent = TRUE)
+
+  x <- gsub("/.*$", "", x)
+  x <- trim(x)
+  x
+}
+
+clear_spec <- function(x) {
+  x <- gsub("[#!$@~\u25ce]+|^[*]", "", x)
+  x <- gsub("\\*", "\\\\*", x)
+  x <-  gsub("^\\s+|[ /-]+$", "", x)
+  x
+}
+data_result_new <- data_result %>%
+  mutate(item_name = mapply(remove_unit, item_name, spec)) %>%
+  mutate(spec = clear_spec(spec))
+
+# save(data_result_new, file="temp/data_result_new.RData")
+
+
+readr::write_csv(data_result_new, "result/data_result.csv")
+
+# hierarchical structure --------------------------------------------------
+
+# load("temp/data_result_new.RData")
+struc <- data_result_new %>%
+  dplyr::select(class_1, class_2, class_3) %>%
+  table() %>%
+  as.data.frame.table(., stringsAsFactors = FALSE) %>%
+  tbl_df %>%
+  filter(Freq!=0) %>%
+  dplyr::select(-Freq) %>%
+  arrange(class_1, class_2, class_3)
+
+write_tsv(struc, "result/ftab.txt")
